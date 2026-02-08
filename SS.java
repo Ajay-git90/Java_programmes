9981ba6e68b4cc9fb05713fca8a79dc5fc2b776e
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-02-09 00:22:11 +0530

    Added

--- SS.java
+++ SS.java
@@ -1,4 +1,4 @@
-import java.net.SocketPermission;
+
 import java.util.Scanner;
 
 class SS {
@@ -64,32 +64,28 @@ class SS {
              * str.equalsIgnoreCase(str1);
              */
         }
-    }   //Main function ends here
+    } // Main function ends here
 
-                     static boolean equals(String s1, String s2)  {
-                       if (s1.length() != s2.length())
-                          return false;
-                            for (int i = 0; i < s1.length(); i++) {
-                               if (s1.charAt(i) != s2.charAt(i)) {
-                              return false;
-                          }
-                       }
-                   return true;
-                          }
-                          
-                        static boolean equalsIgnoreCase(String s1,String s2){
-                           if(s1.length() != s2.length())
-                               return false;
-                                 for (int i=0;i<s1.length();i++){
-                               if(Character.toLowerCase(s1.charAt(i))!= Character.toLowerCase(s2.charAt(i))){
-                              return false;
-                                   }   
-                                     }
-                             return true;
-                            }
+    static boolean equals(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    static boolean equalsIgnoreCase(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (Character.toLowerCase(s1.charAt(i)) != Character.toLowerCase(s2.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-
-
-
-} //class ends
+} // class ends
--- inheritance/Inherit.java
+++ inheritance/Inherit.java
@@ -1,5 +1,45 @@
- class Main{
-    public static void  main (String [] s){
-        System.out.println("hello");
+class rect {
+    int length, breath;
+
+    rect() {}
+
+    rect(int l1, int b1) {
+        length = l1;
+        breath = b1;
+
     }
-} 
+
+    void display() {
+        System.out.println("length....   " + length + "breath....  " + breath);
+                    }
+}
+
+class box extends rect 
+{
+int height;
+    box(){}
+  box (int length,int breath,int height){
+    this.length=length;
+    this.height=height;
+    this.breath=breath;
+  }
+  double area(){
+    int ara=2*(length+breath);
+    return ara;
+  }
+void display()
+    {
+    System.out.println(height);
+    }
+    void volume(){
+       double vol=area()*height;
+        System.out.println(vol);
+    }
+}
+
+
+class Main {
+    public static void main  (String [] s){
+    box b=new box (3,4,6);
+    }
+}
