88487b78e80edee1e4f24d6fbb47ed47a83d525b
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-02-11 00:22:47 +0530

    Q5 added

---
+++ Q5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class SP {
+    public static void main(String[] s) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str = sc.nextLine();
+        System.out.println(str + "\n" +"Length of String :  "+ str.length());
+
+    }
+
+}
