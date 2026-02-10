f6f6bbd314aee7563ad85e62f9190c3555b5ccda
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-02-10 23:49:39 +0530

    Q3 solved and pushed

---
+++ Q3.java
@@ -0,0 +1,13 @@
+ class Welcome 
+{
+    static void showMessage()
+    
+    {
+
+        System.out.println("Welcome to Java Programming");
+    }
+    public static void main (String [] s)
+        {
+        showMessage();
+        }
+}
