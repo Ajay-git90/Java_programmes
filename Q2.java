0246913915f9bcdb2a35396cd2205055a9a22393
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-02-10 20:59:36 +0530

    Practice question Q2 solved and commited

---
+++ Q2.java
@@ -0,0 +1,15 @@
+ class Employee {
+    static int count=0 ;
+    Employee(){
+        count ++;
+    }
+    void display(){
+        System.out.print("Total  Employee : "+count);
+    }
+    public static void main(String [] s){
+      Employee e1=new Employee();
+      Employee e2 =new Employee();
+      Employee e3=new Employee();
+     e1.display();
+    }
+}
