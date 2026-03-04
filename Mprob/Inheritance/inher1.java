2109b4028305ffe3a5c8084007e27e4a4a36d6ef
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-03-04 19:42:51 +0530

    Inheritance q1 added

---
+++ Mprob/Inheritance/inher1.java
@@ -0,0 +1,28 @@
+class Bank
+{
+    void getRateOfInterest()
+    {
+        System.out.println("Generic  Bank Interest");
+    }
+}
+
+class SBI extends  Bank 
+{  void getRateOfInterest(){
+    System.out.println("SBI Interest is 7%");
+}
+}
+class HDFC extends Bank{
+ void getRateOfInterest(){
+    System.out.println("HDFC Interest rate is 8%");
+ }
+}
+  
+class Main  
+{
+    public static void main (String [] s){
+        Bank b1=new  SBI();
+        Bank b2=new HDFC();
+        b1.getRateOfInterest();
+        b2.getRateOfInterest();
+    }
+}
