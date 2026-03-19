4102ba48966f47d54ed7b3343a39a08078f78a4b
Ajay-git90 <ajaykumarcwe9084@gmail.com>
2026-03-19 12:52:53 +0530

    Upadted  Object read write using I/O

--- JavaIO/objwr.java
+++ JavaIO/objwr.java
@@ -1,4 +1,10 @@
-import java.io.* ; 
+  import java.io.* ; 
+/*import java.io.FileNotFoundException ;
+import java.io.IOException ;
+import java.io.FileInputStream ;
+import java.io.FileOutputStream ;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream ;*/
 
 class Com implements Serializable
 {
